6e596952b9627ee1a59dd8f3b1405bf627dd8c9a
Nicholas Steicke <nicholas.steicke@narthollis.net>
2018-06-02 09:50:08 +0930

    Now with history!

--- static/react-status.js
+++ static/react-status.js
@@ -5,6 +5,11 @@ async function getCurrent() {
     return response.json();
 }
 
+async function getHistory() {
+    const response = await fetch('/history');
+    return response.json();
+}
+
 class OverallStatus extends React.PureComponent {
     render() {
         return React.createElement(
@@ -28,9 +33,52 @@ class StatusDisplay extends React.PureComponent {
     }
 }
 
+function *groupHistory(history) {
+    let count = 0;
+    let className = history[0].ClassName;
+    for (const period of history) {
+        if (className !== period.ClassName) {
+            yield {
+                width: count,
+                className: className
+            };
+
+            count = 1;
+            className = period.ClassName;
+        } else {
+            count = count + 1;
+        }
+    }
+
+    yield {
+        width: count,
+        className
+    };
+}
+
+class History extends React.PureComponent {
+    render() {
+        const groupings = [...groupHistory(this.props.history)];
+
+        return React.createElement(
+            'td',
+            { className: 'history' },
+            React.createElement(
+                'div',
+                undefined,
+                ...groupings.map(g => React.createElement(
+                    'div',
+                    { className: `bg-${g.className}`, style: { width: `${g.width}px`} }
+                ))
+            )
+        )
+    }
+}
+
 class Service extends React.PureComponent {
     render() {
         const service = this.props.service;
+        const history = this.props.history;
 
         return React.createElement(
             'tr',
@@ -38,14 +86,16 @@ class Service extends React.PureComponent {
             React.createElement('td', { className: 'service' }, service.Name),
             React.createElement('td', { className: 'counts' }, service.Running, ' / ', service.Replicas),
             React.createElement(StatusDisplay, { element: 'td', status: service.Status }),
-            React.createElement('td', { className: 'history' })
+            history && React.createElement(History, { history })
         );
     }
 }
 
-class Group extends React.PureComponent {
+class Group extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = { showBody: false, prevStatus: null };
 
         this.onClick = this.onClick.bind(this);
     }
@@ -57,6 +107,9 @@ class Group extends React.PureComponent {
     render() {
         const name = this.props.name;
         const group = this.props.group;
+        const history = this.props.history;
+
+        console.log(`${name}-${this.id}`);
 
         return [
             React.createElement(
@@ -67,14 +120,19 @@ class Group extends React.PureComponent {
                     undefined,
                     React.createElement('th', { colSpan: 2, className: 'group-name' }, name),
                     React.createElement(StatusDisplay, { element: 'th', status: group.Status }),
-                    React.createElement('td', { className: 'history' })
+                    history && React.createElement(History, { history: history.History })
                 )
             ),
             this.state.showBody ?
                 React.createElement(
                     'tbody',
                     { key: 'tbody', className: 'table-sm' },
-                    group.Services.map(service => React.createElement(Service, { key: service.ID, service }))
+                    group.Services.map(service =>
+                        React.createElement(
+                            Service,
+                            { key: service.ID, service, history: history && history.Services[service.ID] }
+                        )
+                    )
                 )
             : null
         ]
@@ -82,7 +140,12 @@ class Group extends React.PureComponent {
 }
 
 Group.getDerivedStateFromProps = function (props, prevState) {
-    return { showBody: props.group.Status.Value !== 'Operational' };
+    if (props.group.Status.Value !== prevState.prevStatus) {
+        return {
+            prevStatus: props.group.Status.Value,
+            showBody: props.group.Status.Value !== 'Operational'
+        }
+    }
 };
 
 class Table extends React.PureComponent {
@@ -103,34 +166,81 @@ class Table extends React.PureComponent {
                     React.createElement('th', undefined, 'History'),
                 )
             ),
-            [...Object.entries(this.props.groups)].map(([groupName, group]) => React.createElement(
-                Group,
-                { key: groupName, name: groupName, group }
-            ))
+            React.createElement(
+                React.Fragment,
+                undefined,
+                [...Object.entries(this.props.groups)].map(([groupName, group]) => React.createElement(
+                    Group,
+                    { key: groupName, name: groupName, group, history: this.props.history[groupName] }
+                ))
+            )
         );
     }
 }
+
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
 
 class Root extends React.PureComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            current: null
+            current: null,
+            history: {}
         };
 
         this.loadCurrent = this.loadCurrent.bind(this);
+        this.loadHistory = this.loadHistory.bind(this);
     }
 
     componentDidMount() {
         this.loadCurrent();
-        setInterval(this.loadCurrent, 10000); // 10sec
+        setInterval(this.loadCurrent, 10 * SECOND);
+
+        this.loadHistory();
+        setInterval(this.loadHistory, 5 * MINUTE);
     }
 
     loadCurrent() {
         getCurrent().then(current => {
             this.setState(() => ({ current }))
         });
+    }
+
+    loadHistory() {
+        getHistory().then(history => {
+            const historyById = {};
+
+            for (const period of history) {
+                for (const [groupName, group] of Object.entries(period.Groups)) {
+                    if (historyById[groupName] == null) {
+                        historyById[groupName] = {
+                            History: [],
+                            Services: {},
+                        };
+                    }
+
+                    historyById[groupName].History.push({
+                        timestamp: period.Timestamp,
+                        ...group.Status,
+                    });
+
+                    for (const service of group.Services) {
+                        if (historyById[groupName].Services[service.ID] == null) {
+                            historyById[groupName].Services[service.ID] = [];
+                        }
+
+                        historyById[groupName].Services[service.ID].push({
+                            timestamp: period.Timestamp,
+                            ...service.Status,
+                        });
+                    }
+                }
+            }
+
+            this.setState(() => ({ history: historyById }));
+        })
     }
 
     render() {
@@ -140,7 +250,7 @@ class Root extends React.PureComponent {
 
         const current = this.state.current;
 
-        const lastUpdated =luxon.DateTime.fromISO(current.Timestamp).toFormat("ccc d LLL yyyy HH:mm:ss")
+        const lastUpdated =luxon.DateTime.fromISO(current.Timestamp).toFormat("ccc d LLL yyyy HH:mm:ss");
 
         return [
             React.createElement(
@@ -149,7 +259,7 @@ class Root extends React.PureComponent {
             ),
             React.createElement(
                 Table,
-                { key: 'table', groups: current.Groups },
+                { key: 'table', groups: current.Groups, history: this.state.history },
                 React.createElement(
                     'caption',
                     undefined,
